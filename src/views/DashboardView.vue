631efadc848ff3aec3e47c47bb09a346d515a330
JuanApza <apazajuan004@gmail.com>
2026-01-19 17:27:54 -0500

    Diseño Sidebar

---
+++ src/views/DashboardView.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="">
+        <Tarjetas />
+    </div>
+</template>
+
+<script setup>
+    import Tarjetas from '@/Composables/Dashboard/Tarjetas.vue'
+</script>
---
+++ src/views/SideBar.vue
@@ -0,0 +1,92 @@
+    <template>
+        <div class="sidebar p-8 bg-gray-200 h-full border border-gray-300 rounded-lg flex flex-col">
+            <div class="flex flex-col ">
+                <div class="flex flex-col pb-8 ">
+                    <img 
+                    src="/svg/brand-github-copilot.svg" 
+                    alt="Logo" 
+                    class="logo"/>
+                    <h1 class="title text-2xl font-bold cursor-default text-center">Juan Apaza</h1>
+                </div>
+
+                <p class="text-gray-400 font-semibold hover:cursor-default">Menu</p>
+                <ul class="text-xl flex flex-col gap-2 mt-4">
+                    <li class="
+                    border-l-2 border-transparent pl-2 font-medium text-gray-5
+                    00 text-gray-500
+                    hover:cursor-pointer flex
+                    hover:border-l-2 transition hover:border-green-800">
+                    <svg xmlns="http://www.w3.org/2000/svg" 
+                    fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" 
+                    class="  size-6 mr-2">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+                    </svg>
+                    Home
+                    </li>
+                    <li class="
+                    border-l-2 border-transparent pl-2 font-medium text-gray-5
+                    00  text-gray-500
+                    hover:cursor-pointer flex
+                    hover:border-l-2 transition hover:border-green-800">
+                    <svg xmlns="http://www.w3.org/2000/svg"
+                    fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                    class="  size-6 mr-2">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z" />
+                    </svg>
+
+                        Calendario
+                    </li>
+                    <li class="
+                    border-l-2 border-transparent pl-2 font-medium text-gray-5
+                    00  text-gray-500
+                    hover:cursor-pointer flex
+                    hover:border-l-2 transition hover:border-green-800">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="  size-6 mr-2">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z" />
+                    </svg>
+
+                        Tareas
+                    </li>
+                    <li class="
+                    border-l-2 border-transparent pl-2 font-medium text-gray-5
+                    00  text-gray-500
+                    hover:cursor-pointer flex
+                    hover:border-l-2 transition hover:border-green-800">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 mr-2">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+                    </svg>
+                    Usuario</li>
+                </ul>
+            </div>
+                    <div class="redireccion justify-end mt-auto flex text-white">
+                        <div class="
+                        w-full flex flex-col justify-center bg-cover bg-center items-center
+                        bg-[url(/images/plants.png)] 
+                        p-2 border rounded-lg bg-black/20 bg-blend-overlay">
+                        <svg xmlns="http://www.w3.org/2000/svg" 
+                        x="0px" y="0px" width="100"
+                        class="rounded-full border-0 fill-white "
+                        viewBox="0 0 30 30">
+                            <path d="M15,3C8.373,3,3,8.373,3,15c0,5.623,3.872,10.328,9.092,11.63C12.036,26.468,12,26.28,12,26.047v-2.051 c-0.487,0-1.303,0-1.508,0c-0.821,0-1.551-0.353-1.905-1.009c-0.393-0.729-0.461-1.844-1.435-2.526 c-0.289-0.227-0.069-0.486,0.264-0.451c0.615,0.174,1.125,0.596,1.605,1.222c0.478,0.627,0.703,0.769,1.596,0.769 c0.433,0,1.081-0.025,1.691-0.121c0.328-0.833,0.895-1.6,1.588-1.962c-3.996-0.411-5.903-2.399-5.903-5.098 c0-1.162,0.495-2.286,1.336-3.233C9.053,10.647,8.706,8.73,9.435,8c1.798,0,2.885,1.166,3.146,1.481C13.477,9.174,14.461,9,15.495,9 c1.036,0,2.024,0.174,2.922,0.483C18.675,9.17,19.763,8,21.565,8c0.732,0.731,0.381,2.656,0.102,3.594 c0.836,0.945,1.328,2.066,1.328,3.226c0,2.697-1.904,4.684-5.894,5.097C18.199,20.49,19,22.1,19,23.313v2.734 c0,0.104-0.023,0.179-0.035,0.268C23.641,24.676,27,20.236,27,15C27,8.373,21.627,3,15,3z"></path>
+                        </svg>
+                            <p class="text-sm">Desarrollador <span class="font-bold">Full Stack</span> </p>
+                            <p class="text-sm font-bold">Mis Proyectos</p>
+                            <button class="
+                            mt-4 mb-2 w-full bg-green-800 hover:bg-green-900 
+                            text-white font-medium py-2 px-4 rounded-full transition
+                            hover:cursor-pointer">
+                                Portafolio
+                            </button>
+
+                        </div>
+                    </div>
+        </div>
+    </template>
+
+    <script setup>
+
+    </script>
+
+    <style lang="scss" scoped>
+
+    </style>
