cfde5af052dc6378f189055c976cbd58c6a1f207
JuanApza <apazajuan004@gmail.com>
2026-01-20 04:11:24 -0500

    Header firts

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,14 @@
 import { RouterLink, RouterView } from 'vue-router'
 import DashboardView from './views/DashboardView.vue'
 import SideBar from './views/SideBar.vue';
-import Header from './views/Header.vue';
+import Headerview from './views/Headerview.vue';
 </script>
 
 <template>
-  <div class="flex h-screen p-4">
+  <div class="flex h-screen p-8 gap-4">
     <SideBar />
     <div class="flex flex-col flex-1">
-      <Header />
+      <Headerview />
       <main class="p-4 overflow-auto">
         <RouterView />
       </main>
---
+++ src/views/Headerview.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="w-full p-6 bg-gray-200 border border-gray-300 rounded-lg flex flex-row justify-between items-center gap-4">
+        
+        <div class="buscador bg-white w-4/5 rounded-full flex flex-row px-4 py-2 gap-2">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+            </svg>
+            <input class="w-full outline-none focus:outline-none" type="text" placeholder="Buscar Tarea">
+        </div>
+        <div class="flex flex-row gap-2 items-center">
+            <div class="bg-white p-1.5 rounded-full hover:bg-gray-100 cursor-pointer">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
+                </svg>
+            </div>
+            <div class="bg-white p-1.5 rounded-full hover:bg-gray-100 cursor-pointer">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0" />
+                </svg>
+
+            </div>
+            <div class="flex items-center gap-2 ">
+                <div class="bg-white p-1.5 rounded-full hover:bg-gray-100 cursor-pointer">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                    class="size-6 stroke-current">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+                    </svg>
+                </div>
+                <div class="flex flex-col">
+                    <span class="text-sm font-medium">Juan Perez</span>
+                    <span class="text-xs text-gray-500">Administrador</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
